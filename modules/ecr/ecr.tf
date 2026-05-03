cd9216e395bea2de8b7af5b695741688956096ac
VOSolyanik <v.o.solyanik@gmail.com>
2026-05-04 02:57:43 +0300

    fix: trivy CI findings fix

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_ecr_repository" "this" {
   name                 = var.ecr_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
